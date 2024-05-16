c72f7c823032e4433c957fa6a8fca58c8e6b96a4
sabbirHossain <sabbirshnt@gmail.com>
2024-05-16 10:29:19 +0600

    student interface and model

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,48 @@
+import { Schema, model } from 'mongoose';
+import {
+  TGuardian,
+  TLocalGuardian,
+  TStudent,
+  TUserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<TGuardian>({
+  fatherName: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<TLocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<TStudent>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['female', 'male'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+export const StudentModel = model<TStudent>('Student', studentSchema);
